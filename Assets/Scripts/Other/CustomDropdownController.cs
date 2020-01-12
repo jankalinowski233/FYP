b0f517894162cda13405a14bf75f3f3b0a415900
jankalinowski233 <jan.kalinowski1998@gmail.com>
2020-01-12 21:39:10 +0000

    Imrpoved UI.

---
+++ Assets/Scripts/Other/CustomDropdownController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomDropdownController : MonoBehaviour
+{
+    public GameObject buttonHolder;
+    bool show = false;
+
+    public void ShowButtons()
+    {
+        
+        show = !show;
+        buttonHolder.SetActive(show);
+    }
+}
