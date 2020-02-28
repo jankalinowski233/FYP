a7aaf69ad11397c58a75672ba823e75fce20f9cb
jankalinowski233 <jan.kalinowski1998@gmail.com>
2020-02-28 17:00:55 +0000

    Finished putting 3rd level environment.

--- Assets/Scripts/Other/EventTrigger.cs
+++ Assets/Scripts/Other/EventTrigger.cs
@@ -6,10 +6,14 @@ using UnityEngine.Events;
 public class EventTrigger : MonoBehaviour
 {
     public UnityEvent OnPlayerTrigger;
+    public UnityEvent OnDestructableTrigger;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
             OnPlayerTrigger.Invoke();
+
+        if (other.CompareTag("Destructable"))
+            OnDestructableTrigger.Invoke();          
     }
 }
